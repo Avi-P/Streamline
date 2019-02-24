cfe2fe6f80cf9d1e36624a2791e04b407f0ad589
Avi-P <apooruli@gmail.com>
2019-02-24 05:42:32 -0500

    Add listener to get url everytime the user opens a new page/goes to a new site

--- Streamline/background.js
+++ Streamline/background.js
@@ -30,9 +30,37 @@ chrome.tabs.onActivated.addListener(({tabId, windowId}) => {
                 title: "aids",
                 message: domain
         };
-            
-        console.log(domain);
         
-        chrome.notifications.create('limitNotif', notifOptions);
+        chrome.notifications.create('notif', notifOptions);
     });
+})
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    var url = tab.url;
+        
+    var hostname;
+
+    if (url.indexOf("//") > -1) {
+        hostname = url.split('/')[2];
+    } else {
+        hostname = url.split('/')[0];
+    }
+
+    var domain;
+
+    if (hostname.indexOf("www") > -1) {
+        domain = hostname.split(".")[1];
+    } else {
+        domain = hostname.split(".")[0];
+    }
+
+    var notifOptions = {
+            type: "basic",
+            iconUrl: "icon48.png",
+            title: "aids",
+            message: domain
+    };
+        
+    
+    chrome.notifications.create('notif', notifOptions);
 })
